a796ec2cd0cbe73c8e91bbb40cf22bed47519c1d
liyinqin <502359058@qq.com>
2018-09-20 10:32:50 +0800

    1

---
+++ js/index.20180920103241.js
@@ -0,0 +1,9 @@
+(function(d){var h=[];d.loadImages=function(a,e){"string"==typeof a&&(a=[a]);for(var f=a.length,g=0,b=0;b<f;b++){var c=document.createElement("img");c.onload=function(){g++;g==f&&d.isFunction(e)&&e()};c.src=a[b];h.push(c)}}})(window.jQuery);
+$.fn.hasAttr = function(name) { var attr = $(this).attr(name); return typeof attr !== typeof undefined && attr !== false; };
+
+
+$(document).ready(function() {
+r=function(){dpi=window.devicePixelRatio;$('.js').attr('src', (dpi>1) ? 'images/ea8e2369911995.5b914ee7d4a85-1354.jpg' : 'images/ea8e2369911995.5b914ee7d4a85-677.jpg');};
+if(!window.HTMLPictureElement){r();}
+
+});
